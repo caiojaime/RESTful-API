f14a736aee84dc7f6d2da8d644fae439ca66c878
Caio Jaime <kobashijaime@gmail.com>
2020-08-26 21:19:26 -0300

    Project structure code.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+let app = express();
+/* bodyParser.urlencoded({}) codifica a URL (transfora em URI) para conseguirmos
+    retornar um arquivo JSON (bodyParser.json()) e manipularmos da forma que quisermos.
+*/
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(expressValidator());
+
+// Modulo Consign importa todas as rotas da pasta "routes" e inclui  a variável app dentro de cada rota
+consign().include('routes').include('utils').into(app);
+
+app.listen(4000, '127.0.0.1', () =>{
+
+console.log("Servidor rodando");
+
+});
